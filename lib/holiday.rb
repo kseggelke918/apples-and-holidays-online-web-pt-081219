bcb9c5397b1182d04fc2242329a8502a794da3ef
Katie Seggelke <katie.seggelke@gmail.com>
2019-08-27 22:46:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -100,6 +100,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holidays|
     holidays.collect do |holiday, supplies|
       if supplies.include? ("BBQ")
+        holiday 
       end 
     end 
   end 
